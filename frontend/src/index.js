3b9e1ba32c3b57d17dfb0602deff0f8eb7e21eca
Masha Moreva <mashamiskova@yandex.ru>
2023-12-22 19:16:26 +0300

    update-index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,39 @@
+import { el, setChildren } from "redom";
+import Navigo from "navigo";
+import "./styles/styles.scss";
+import { createHeader } from "./header.js";
+import { createLoginForm } from "./login.js";
+
+const mainContainer = el("main");
+const headerContainer = createHeader();
+setChildren(document.body, [headerContainer, mainContainer]);
+
+const router = new Navigo(null, true, "#");
+
+function checkAuthorization() {
+  const isAuthorized = localStorage.getItem("isAuthorized");
+  const route = isAuthorized === "true" ? "/accounts" : "/login";
+  router.navigate(route);
+}
+
+checkAuthorization();
+
+router
+  .on("/login", () => {
+    setChildren(mainContainer, [createLoginForm()]);
+  })
+  .on("/accounts", () => {
+    setChildren(mainContainer, [el("h1", "Страница счетов пользователя")]);
+  })
+  .on("/account/:id", (params) => {
+    setChildren(mainContainer, [
+      el("h1", `Страница деталей счета с id: ${params.id}`),
+    ]);
+  })
+  .on("/currency", () => {
+    setChildren(mainContainer, [el("h1", "Страница валюты")]);
+  })
+  .on("/atm-map", () => {
+    setChildren(mainContainer, [el("h1", "Страница с картой банкоматов")]);
+  })
+  .resolve();
