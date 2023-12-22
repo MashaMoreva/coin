af16f58294ab55b1aa7c26a22352f34d62d2689e
Masha Moreva <mashamiskova@yandex.ru>
2023-12-22 19:09:16 +0300

    add-html-webpack-plugin-modules-for-images-and-scss-configure-devServer

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,32 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ttf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Coin.",
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
   },
 };
