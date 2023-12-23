7fec2120dae3934fa4ecdb3ddd907f3e0b41d1d6
Masha Moreva <mashamiskova@yandex.ru>
2023-12-23 15:23:46 +0300

    add-fieldset-tag-for-styling

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,24 +1,31 @@
 import { el } from "redom";
+import "./login.scss";
 
 export function createLoginForm() {
-  const loginContainer = el(".login", [
+  const loginContainer = el("div.login", [
     el("h1.login-title", "Вход в аккаунт"),
     el("form.login-form", { id: "loginForm" }, [
-      el("label.login-label", { for: "username" }, "Логин:"),
-      el("input.login-input", {
-        type: "text",
-        id: "username",
-        name: "username",
-        required: true,
-      }),
+      el("fieldset.login-fieldset", [
+        el("label.login-label", { for: "username" }, "Логин:"),
+        el("input.login-input", {
+          type: "text",
+          id: "username",
+          name: "username",
+          placeholder: "Введите логин",
+          required: true,
+        }),
+      ]),
 
-      el("label.login-label", { for: "password" }, "Пароль:"),
-      el("input.login-input", {
-        type: "password",
-        id: "password",
-        name: "password",
-        required: true,
-      }),
+      el("fieldset.login-fieldset", [
+        el("label.login-label", { for: "password" }, "Пароль:"),
+        el("input.login-input", {
+          type: "password",
+          id: "password",
+          name: "password",
+          placeholder: "Введите пароль",
+          required: true,
+        }),
+      ]),
 
       el("button.login-button", { type: "submit" }, "Войти"),
     ]),
