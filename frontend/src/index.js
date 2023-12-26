8d2e48e7b2db4cab95c2284fce8c8f415b8301b9
Masha Moreva <mashamiskova@yandex.ru>
2023-12-26 13:13:13 +0300

    refactor: improve-token-auth-check

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,36 +4,37 @@ import "./styles/styles.scss";
 import { createHeader } from "./components/header/header.js";
 import { createLoginForm } from "./components/login/login.js";
 
-const mainContainer = el("main");
-const headerContainer = createHeader();
-setChildren(document.body, [headerContainer, mainContainer]);
-
 const router = new Navigo(null, true, "#");
 
 function checkAuthorization() {
-  const isAuthorized = localStorage.getItem("isAuthorized");
-  const route = isAuthorized === "true" ? "/accounts" : "/login";
+  const token = localStorage.getItem("token");
+  const isAuthorized = !!token;
+  const route = isAuthorized ? "/accounts" : "/login";
   router.navigate(route);
+
+  const mainContainer = el("main");
+  const headerContainer = createHeader(isAuthorized, router);
+  setChildren(document.body, [headerContainer, mainContainer]);
+
+  router
+    .on("/login", () => {
+      setChildren(mainContainer, [createLoginForm(router)]);
+    })
+    .on("/accounts", () => {
+      setChildren(mainContainer, [el("h1", "Страница счетов пользователя")]);
+    })
+    .on("/account/:id", (params) => {
+      setChildren(mainContainer, [
+        el("h1", `Страница деталей счета с id: ${params.id}`),
+      ]);
+    })
+    .on("/currency", () => {
+      setChildren(mainContainer, [el("h1", "Страница валюты")]);
+    })
+    .on("/atm-map", () => {
+      setChildren(mainContainer, [el("h1", "Страница с картой банкоматов")]);
+    })
+    .resolve();
 }
 
 checkAuthorization();
-
-router
-  .on("/login", () => {
-    setChildren(mainContainer, [createLoginForm(router)]);
-  })
-  .on("/accounts", () => {
-    setChildren(mainContainer, [el("h1", "Страница счетов пользователя")]);
-  })
-  .on("/account/:id", (params) => {
-    setChildren(mainContainer, [
-      el("h1", `Страница деталей счета с id: ${params.id}`),
-    ]);
-  })
-  .on("/currency", () => {
-    setChildren(mainContainer, [el("h1", "Страница валюты")]);
-  })
-  .on("/atm-map", () => {
-    setChildren(mainContainer, [el("h1", "Страница с картой банкоматов")]);
-  })
-  .resolve();
