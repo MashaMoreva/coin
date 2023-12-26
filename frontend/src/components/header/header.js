45588258c7062ffd57187a0cacd01e4e76f8b015
Masha Moreva <mashamiskova@yandex.ru>
2023-12-26 13:16:31 +0300

    feat: add-navigation-buttons

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,7 +1,35 @@
 import { el } from "redom";
 import logo from "../../assets/images/logo.svg";
 import "./header.scss";
+import "../../styles/colors.scss";
+import { createButton } from "../button/button";
 
-export const createHeader = () => {
-  return el("header.header", [el("img.header-logo", { src: logo })]);
+export const createHeader = (isAuthorized, router) => {
+  const buttons = isAuthorized
+    ? [
+        createButton({
+          text: "Банкоматы",
+          onClick: () => router.navigate("/atm-map"),
+          reversedColors: true,
+        }),
+        createButton({
+          text: "Счета",
+          onClick: () => router.navigate("/accounts"),
+          reversedColors: true,
+        }),
+        createButton({
+          text: "Валюта",
+          onClick: () => router.navigate("/currency"),
+          reversedColors: true,
+        }),
+        createButton({ text: "Выйти", reversedColors: true }),
+      ]
+    : [];
+
+  const buttonsWrapper = el("div.header-wrapper", buttons);
+
+  return el("header.header", [
+    el("img.header-logo", { src: logo }),
+    buttonsWrapper,
+  ]);
 };
