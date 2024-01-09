30863c5a80461942cf938dadda67ea39b5a3e960
Masha Moreva <mashamiskova@yandex.ru>
2024-01-09 15:18:15 +0300

    fix-last-transaction

--- frontend/src/components/accounts/accountCard/accountCard.js
+++ frontend/src/components/accounts/accountCard/accountCard.js
@@ -4,13 +4,19 @@ import { createButton } from "../../button/button";
 import { formatDate } from "../../../helpers/formatDate";
 
 export function createAccountCard(account) {
+  let lastTransactionDate = "Нет транзакций";
+
+  if (account.transactions && account.transactions.length > 0) {
+    lastTransactionDate = formatDate(account.transactions[0].date);
+  }
+
   const card = el("div.card", [
     el("p.card-account", account.account),
     el("p.card-balance", `${account.balance} ₽`),
     el("div.card-info", [
       el("div.card-wrapper", [
         el("p.card-subtitle", "Последняя транзакция:"),
-        el("p.card-date", formatDate(account.transactions[0].date)),
+        el("p.card-date", lastTransactionDate),
       ]),
       createButton({
         text: "Открыть",
