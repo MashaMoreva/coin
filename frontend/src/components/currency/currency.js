7918c60925d20b5d108db324fffea017390a4280
Masha Moreva <mashamiskova@yandex.ru>
2024-01-10 18:53:57 +0300

    update-add-user-currencies

--- frontend/src/components/currency/currency.js
+++ frontend/src/components/currency/currency.js
@@ -1,14 +1,40 @@
-import { el } from "redom";
+import { el, mount } from "redom";
 import "./currency.scss";
 import {
-  getAllCurrencies,
   getUserCurrencies,
+  getAllCurrencies,
   buyCurrency,
 } from "../../helpers/api";
 
 export function createCurrency() {
+  const userCurrenciesContainer = el("div.user-currencies");
+
+  getUserCurrencies()
+    .then((userCurrencies) => {
+      for (const currencyCode in userCurrencies) {
+        const currencyBalance = userCurrencies[currencyCode].amount;
+
+        const currencyElement = el("div.user-currency", [
+          el("span.currency-code", currencyCode),
+          el("span.currency-pass"),
+          el("span.currency-balance", currencyBalance),
+        ]);
+
+        mount(userCurrenciesContainer, currencyElement);
+      }
+    })
+    .catch((error) => {
+      console.error("Ошибка при получении данных о валютах:", error);
+    });
+
   const currencyContainer = el("div.currency", [
     el("h1.currency-title", "Валютный обмен"),
+    el("div.currency-wrapper", [
+      el("div.user", [
+        el("p.user-subtitle", "Ваши валюты"),
+        userCurrenciesContainer,
+      ]),
+    ]),
   ]);
 
   return currencyContainer;
