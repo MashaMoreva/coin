f6c8f50efddf7d9fa5ebfda04c96337a0c355b40
Masha Moreva <mashamiskova@yandex.ru>
2024-01-12 18:42:14 +0300

    update-add-logout

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -22,11 +22,20 @@ export function createHeader(isAuthorized, router) {
           onClick: () => router.navigate("/currency"),
           reversedColors: true,
         }),
-        createButton({ text: "Выйти", reversedColors: true }),
+        createButton({
+          text: "Выйти",
+          reversedColors: true,
+          onClick: handleLogout,
+        }),
       ]
     : [];
 
   const buttonsWrapper = el("div.header-wrapper", buttons);
+
+  function handleLogout() {
+    localStorage.removeItem("token");
+    router.navigate("/login");
+  }
 
   return el("header.header", [
     el("img.header-logo", { src: logo }),
