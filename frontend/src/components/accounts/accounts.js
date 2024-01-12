d73e11f08f4e6a813c78b7ad5b0b5e0b9852f5a5
Masha Moreva <mashamiskova@yandex.ru>
2024-01-12 13:43:11 +0300

    isolated-dropdown-component

--- frontend/src/components/accounts/accounts.js
+++ frontend/src/components/accounts/accounts.js
@@ -3,17 +3,16 @@ import { el, mount } from "redom";
 import { createAccountCard } from "./accountCard/accountCard";
 import { createButton } from "../button/button";
 import { createNewAccount, getAccounts } from "../../helpers/api";
+import { createDropdownSelect } from "../dropdownSelect/dropdownSelect";
 
 export function createAccounts(router) {
   const accountsContainer = el("div.accounts", [
     el("div.accounts-controls", [
       el("h1.accounts-controls-title", "Ваши счета"),
-      el("div.accounts-controls-select", [
-        el("input.accounts-controls-input", {
-          value: "Сортировка",
-          readOnly: true,
-        }),
-        el("ul.accounts-controls-options"),
+      createDropdownSelect([
+        "По номеру",
+        "По балансу",
+        "По последней транзакции",
       ]),
       createButton({
         text: "Создать новый счёт",
@@ -36,34 +35,6 @@ export function createAccounts(router) {
       mount(accountCardsContainer, accountCard);
     });
   });
-
-  const select = accountsContainer.querySelector(".accounts-controls-select");
-  const selectOptions = accountsContainer.querySelector(
-    ".accounts-controls-options"
-  );
-  const input = accountsContainer.querySelector(".accounts-controls-input");
-
-  const optionsData = ["По номеру", "По балансу", "По последней транзакции"];
-  let selectedOption = null;
-
-  function toggleOptions() {
-    select.classList.toggle("open");
-  }
-
-  optionsData.forEach((option) => {
-    const optionElement = el("li.accounts-controls-option", option);
-    selectOptions.appendChild(optionElement);
-    optionElement.addEventListener("click", () => {
-      input.value = option;
-      if (selectedOption) {
-        selectedOption.classList.remove("checked");
-      }
-      optionElement.classList.add("checked");
-      selectedOption = optionElement;
-    });
-  });
-
-  select.addEventListener("click", toggleOptions);
 
   return accountsContainer;
 }
