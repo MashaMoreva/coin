40ac08057ad5ce4e3866b746f015082167d72f71
Masha Moreva <mashamiskova@yandex.ru>
2024-01-12 15:57:23 +0300

    update-add-props

--- frontend/src/components/dropdownSelect/dropdownSelect.js
+++ frontend/src/components/dropdownSelect/dropdownSelect.js
@@ -1,11 +1,20 @@
 import { el } from "redom";
 import "./dropdownSelect.scss";
 
-export function createDropdownSelect(optionsData, onSelect) {
+export function createDropdownSelect(
+  optionsData,
+  labelText,
+  readOnly = true,
+  placeholder = "Введите счёт"
+) {
+  const defaultValue = optionsData.length > 0 ? optionsData[0] : "";
+
   const select = el("div.dropdown-select", [
+    el("div.dropdown-label", labelText),
     el("input.dropdown-input", {
-      value: "Сортировка",
-      readOnly: true,
+      value: defaultValue,
+      placeholder: placeholder,
+      readOnly,
     }),
     el("ul.dropdown-options"),
   ]);
@@ -17,9 +26,15 @@ export function createDropdownSelect(optionsData, onSelect) {
     select.classList.toggle("open");
   }
 
-  optionsData.forEach((option) => {
+  optionsData.forEach((option, index) => {
     const optionElement = el("li.dropdown-option", option);
     selectOptions.appendChild(optionElement);
+
+    if (option === defaultValue) {
+      optionElement.classList.add("checked");
+      selectedOption = optionElement;
+    }
+
     optionElement.addEventListener("click", () => {
       select.querySelector(".dropdown-input").value = option;
       if (selectedOption) {
