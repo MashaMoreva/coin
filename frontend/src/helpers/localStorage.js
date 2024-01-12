8a53386231763dde79c1a06849a0cf68290da935
Masha Moreva <mashamiskova@yandex.ru>
2024-01-12 13:42:06 +0300

    create-localStorage-helper

---
+++ frontend/src/helpers/localStorage.js
@@ -0,0 +1,8 @@
+export function getFromLocalStorage(key) {
+  const data = localStorage.getItem(key);
+  return data ? JSON.parse(data) : null;
+}
+
+export function saveToLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
