55c4cdc91a72aba0fbee1bece1e3db2a40103fd1
Masha Moreva <mashamiskova@yandex.ru>
2024-01-24 22:06:50 +0300

    fix: declared-authentication

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,43 +8,39 @@ import { createAccount } from "./components/account/account.js";
 import { createCurrency } from "./components/currency/currency.js";
 
 const router = new Navigo(null, true, "#");
-let isAuthorized = false;
 
 function checkAuthorization() {
-  const token = localStorage.getItem("token");
-  isAuthorized = !!token;
-
   router
     .on("/login", () => {
-      if (!isAuthorized) {
+      if (!localStorage.getItem("token")) {
         createLoginForm(router);
       } else {
         router.navigate("/accounts");
       }
     })
     .on("/accounts", () => {
-      if (isAuthorized) {
+      if (!!localStorage.getItem("token")) {
         createAccounts(router);
       } else {
         router.navigate("/login");
       }
     })
     .on("/account/:id", (params) => {
-      if (isAuthorized) {
+      if (localStorage.getItem("token")) {
         createAccount(params.data.id, router);
       } else {
         router.navigate("/login");
       }
     })
     .on("/currency", () => {
-      if (isAuthorized) {
+      if (localStorage.getItem("token")) {
         createCurrency(router);
       } else {
         router.navigate("/login");
       }
     })
     .on("/map", () => {
-      if (isAuthorized) {
+      if (localStorage.getItem("token")) {
         createMap(router);
       } else {
         router.navigate("/login");
