7da2df6673ffb694e4fb6acfcaa91b03a09a5897
Masha Moreva <mashamiskova@yandex.ru>
2024-01-24 14:46:11 +0300

    update: function-logic

--- frontend/src/helpers/updateAccounts.js
+++ frontend/src/helpers/updateAccounts.js
@@ -1,7 +1,9 @@
 import { createAccountCard } from "../components/accounts/accountCard/accountCard";
+import { setChildren } from "redom";
 
-export function updateAccounts(userAccounts, accountCardsContainer) {
-  accountCardsContainer.innerHTML = userAccounts
-    .map((account) => createAccountCard(account).outerHTML)
-    .join("");
+export function updateAccounts(userAccounts, accountCardsContainer, router) {
+  const newAccountCards = userAccounts.map((account) =>
+    createAccountCard(account, router)
+  );
+  setChildren(accountCardsContainer, newAccountCards);
 }
