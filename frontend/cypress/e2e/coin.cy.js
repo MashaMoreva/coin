83a56a5f6d34d557a088f9bb75ca0ddfbd7d6abd
Masha Moreva <mashamiskova@yandex.ru>
2024-01-29 20:02:34 +0300

    update: tests

--- frontend/cypress/e2e/coin.cy.js
+++ frontend/cypress/e2e/coin.cy.js
@@ -1,28 +1,46 @@
 describe("Веб-приложение Coin.", () => {
   beforeEach(() => {
     cy.clearLocalStorage();
+    cy.request({
+      method: "POST",
+      url: "http://localhost:3000/login",
+      body: {
+        login: "developer",
+        password: "skillbox",
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      localStorage.setItem("token", response.body.payload.token);
+    });
     cy.visit("http://localhost:8080/");
   });
 
-  it("должно отобразить форму входа при отсутствии токена", () => {
-    cy.get(".login-title").should("contain", "Вход в аккаунт");
-  });
-
-  it("должно перенаправить на страницу счетов при наличии токена", () => {
-    cy.get("#username").type("developer");
-    cy.get("#password").type("skillbox");
-    cy.get(".login-button").click();
+  it("должно перенаправить на страницу счетов после успешной авторизации", () => {
     cy.url().should("include", "/accounts");
   });
-  it("должно отобразить список счетов после входа", () => {
-    cy.get("#username").type("developer");
-    cy.get("#password").type("skillbox");
-    cy.get(".login-button").click();
 
-    cy.url().should("include", "/accounts");
-
+  it("должно отобразить список счетов", () => {
     cy.get(".accounts-controls-title").should("contain", "Ваши счета");
-
     cy.get(".card").should("exist");
+  });
+
+  it("должно создать новый счёт", () => {
+    cy.get(".card")
+      .its("length")
+      .then((initialCount) => {
+        cy.contains(".accounts-controls-button", "Создать новый счёт").click();
+        cy.get(".card").should("have.length", initialCount + 1);
+      });
+  });
+
+  it("должно осуществить перевод", () => {
+    cy.contains(".card", "74213041477477406320783754")
+      .find(".card-button")
+      .click();
+    cy.get(".dropdown-select input").type("5555341244441115");
+    cy.get('input[name="amount"]').type("11.11");
+    cy.get(".account-wrapper-form-button").click();
+    cy.get(".account-table-cell").contains("5555341244441115").should("exist");
+    cy.get(".account-table-cell").contains("11.11").should("exist");
   });
 });
